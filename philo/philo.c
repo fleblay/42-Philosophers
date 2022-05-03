af4206d2d4be2e9b57fb1c00c4a2be5fd77b670f
Frederic LE BLAY <fle-blay@e2r1p5.clusters.42paris.fr>
2022-05-03 17:59:58 +0200

    fix leaks if init error

--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: fle-blay <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/02 09:49:35 by fle-blay          #+#    #+#             */
-/*   Updated: 2022/05/03 17:36:20 by fle-blay         ###   ########.fr       */
+/*   Updated: 2022/05/03 17:58:58 by fle-blay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,16 @@ int	ft_init_data(t_data *data, int ac, char *av[])
 {
 	if (ac != 5 && ac != 6)
 		return (ft_putstr_fd("Error : wrong arg count\n", 2), 0);
-	if (!ft_get_param(data, ac, av) || !ft_allocate(data))
-		return (ft_putstr_fd("Error : wrong parameter \n", 2), 0);
+	if (!ft_get_param(data, ac, av))
+		return (ft_putstr_fd("Error : wrong parameter\n", 2), 0);
 	if (data->philo_count > 200 || data->ttd < 60
 		|| data->tte < 60 || data->tts < 60)
-		return (ft_putstr_fd("Error : out of bound parameter \n", 2), 0);
+		return (ft_putstr_fd("Error : out of bound parameter\n", 2), 0);
+	if (!ft_allocate(data))
+		return (ft_putstr_fd("Error : init failure\n", 2), 0);
 	if (!ft_mutex_init(data))
 		return (ft_deallocate(data),
-			ft_putstr_fd("Error : init mutexes \n", 2), 0);
+			ft_putstr_fd("Error : init mutexes\n", 2), 0);
 	ft_set_data(data);
 	return (1);
 }
