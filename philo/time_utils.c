db410ed1c2853966fbd693f402828ea0fe9316f1
Fred <frederic.leblay@gmail.com>
2022-05-11 12:41:12 +0200

    Normal and bonus ok. Norme to do and massive tests

--- philo/time_utils.c
+++ philo/time_utils.c
@@ -6,7 +6,7 @@
 /*   By: fle-blay <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/02 15:25:00 by fle-blay          #+#    #+#             */
-/*   Updated: 2022/05/11 12:04:47 by fred             ###   ########.fr       */
+/*   Updated: 2022/05/11 12:38:57 by fred             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,16 +43,15 @@ int	ft_self_is_dead(t_philo *philo)
 int	ft_eat(t_philo *philo)
 {
 	//pthread_mutex_lock(&philo->data->m_time);
-	philo->current_time = ft_get_time() - philo->start_time;
+	//philo->current_time = ft_get_time() - philo->start_time;
 	//pthread_mutex_unlock(&philo->data->m_time);
+	if (ft_self_is_dead(philo))
+		return (ft_release_forks(philo), 0);
 	s_print(philo, "is eating", 0);
 	philo->last_start_eat = philo->current_time;
 	while (philo->current_time - philo->last_start_eat < philo->tte)
 	{
-		pthread_mutex_lock(&philo->data->m_dead);
-		philo->dead = philo->data->dead_philo;
-		pthread_mutex_unlock(&philo->data->m_dead);
-		if (philo->dead)
+		if (ft_self_is_dead(philo))
 			return (ft_release_forks(philo), 0);
 		else
 			usleep(500);
@@ -70,17 +69,16 @@ int	ft_eat(t_philo *philo)
 int	ft_sleep(t_philo *philo)
 {
 	//pthread_mutex_lock(&philo->data->m_time);
-	philo->current_time = ft_get_time() - philo->start_time;
+	//philo->current_time = ft_get_time() - philo->start_time;
 	//pthread_mutex_unlock(&philo->data->m_time);
+	if (ft_self_is_dead(philo))
+		return (ft_release_forks(philo), 0);
 	s_print(philo, "is sleeping", 0);
 	philo->last_start_sleep = philo->current_time;
 	while (philo->current_time - philo->last_start_sleep < philo->tts)
 	{
-		pthread_mutex_lock(&philo->data->m_dead);
-		philo->dead = philo->data->dead_philo;
-		pthread_mutex_unlock(&philo->data->m_dead);
-		if (philo->dead)
-			return (0);
+		if (ft_self_is_dead(philo))
+			return (ft_release_forks(philo), 0);
 		else
 			usleep(500);
 		//pthread_mutex_lock(&philo->data->m_time);
@@ -93,8 +91,10 @@ int	ft_sleep(t_philo *philo)
 int	ft_think(t_philo *philo)
 {
 	//pthread_mutex_lock(&philo->data->m_time);
-	philo->current_time = ft_get_time() - philo->start_time;
+	//philo->current_time = ft_get_time() - philo->start_time;
 	//pthread_mutex_unlock(&philo->data->m_time);
+	if (ft_self_is_dead(philo))
+		return (ft_release_forks(philo), 0);
 	s_print(philo, "is thinking", 0);
 	philo->last_start_think = philo->current_time;
 	if (philo->ttd < philo->tte + philo->tts + 10)
@@ -102,11 +102,8 @@ int	ft_think(t_philo *philo)
 	while (philo->current_time - philo->last_start_think
 		< philo->ttd - philo->tte - philo->tts - 10)
 	{
-		pthread_mutex_lock(&philo->data->m_dead);
-		philo->dead = philo->data->dead_philo;
-		pthread_mutex_unlock(&philo->data->m_dead);
-		if (philo->dead)
-			return (0);
+		if (ft_self_is_dead(philo))
+			return (ft_release_forks(philo), 0);
 		else
 			usleep(500);
 		//pthread_mutex_lock(&philo->data->m_time);
